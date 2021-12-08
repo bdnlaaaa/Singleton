7075f1204bfb5a45d77d8deb99bfe8a4a596f4ec
bdnlaaaa <95738538+bdnlaaaa@users.noreply.github.com>
2021-12-08 11:05:47 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Administrator
+    {
+        static Administrator administrator;
+        protected Administrator()
+        {
+            Console.WriteLine("Hi! New administrator");
+        }
+        public static Administrator Instance()
+        {
+            if (administrator == null)
+                administrator = new Administrator();
+            else
+                Console.WriteLine("The administrator is already registered");
+            return administrator;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Administrator u = new Administrator();
+            Administrator a = Administrator.Instance();
+            Administrator b = Administrator.Instance();
+        }
+    }
+}
